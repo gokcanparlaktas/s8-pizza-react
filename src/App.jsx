51bb64e7c721cfc8128e62a7b665e23aab6a05ae
Gokcan Parlaktas <gokcanparlaktas@hotmail.com>
2024-11-04 17:16:16 +0300

    Project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import workintech from "/workintech.svg";
+import pizza from "./assets/pizza.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
+        <a
+          href="https://github.com/Workintech/fsweb-s7-challenge-pizza"
+          target="_blank"
+        >
           <img src={workintech} className="logo" alt="Workintech logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={pizza} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Workintech + 🍕</h1>
@@ -21,15 +28,13 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           Absolute Acı Pizza sayısı {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>Your pizza is getting ready, be patient :)</p>
       </div>
       <p className="read-the-docs">
         Click on the Workintech or Pizza logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
