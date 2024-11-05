21e4022abd0b85fba25f020710fc6169d805bffa
Gokcan Parlaktas <gokcanparlaktas@hotmail.com>
2024-11-05 09:32:54 +0300

    Header added, css updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import workintech from "/workintech.svg";
-import pizza from "./assets/pizza.svg";
+import "./reset.css";
 import "./App.css";
+import { Header } from "./Components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a
-          href="https://github.com/Workintech/fsweb-s7-challenge-pizza"
-          target="_blank"
-        >
-          <img src={workintech} className="logo" alt="Workintech logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a
-          href="https://github.com/gokcanparlaktas/pizzachallenge"
-          target="_blank"
-        >
-          <img src={pizza} className="logo pizza" alt="Pizza logo" />
-        </a>
-      </div>
-      <h1>Workintech + 🍕</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Absolute Acı Pizza sayısı {count}
-        </button>
-        <p>Your pizza is getting ready, be patient :)</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Workintech or Pizza logos to learn more
-      </p>
-    </>
+    <header>
+      <Header />
+    </header>
   );
 }
 
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <div className="header">
+      <h1>
+        Teknolojik <br /> Yemekler
+      </h1>
+    </div>
+  );
+}
