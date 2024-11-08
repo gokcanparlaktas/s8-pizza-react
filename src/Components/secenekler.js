7adc2e500b02fc77b2db79db60a37065b0b5db34
Gokcan Parlaktas <gokcanparlaktas@hotmail.com>
2024-11-08 22:36:48 +0300

    Order form component created, imported in App.jsx, datas are stored in secenekler.js and imported in order form component. Form is done completely

---
+++ src/Components/secenekler.js
@@ -0,0 +1,28 @@
+export const sizes = [
+  { id: "s", boy: "Küçük" },
+  { id: "m", boy: "Orta" },
+  { id: "l", boy: "Büyük" },
+];
+
+export const kalinlik = [
+  { id: "i", size: "İnce" },
+  { id: "n", size: "Normal" },
+  { id: "k", size: "Kalın" },
+];
+
+export const ekstralar = [
+  { id: 1, name: "Pepperoni" },
+  { id: 2, name: "Domates" },
+  { id: 3, name: "Biber" },
+  { id: 4, name: "Sosis" },
+  { id: 5, name: "Mısır" },
+  { id: 6, name: "Sucuk" },
+  { id: 7, name: "Kanada Jambonu" },
+  { id: 8, name: "Ananas" },
+  { id: 9, name: "Tavuk Izgara" },
+  { id: 10, name: "Jalepeno" },
+  { id: 11, name: "Kabak" },
+  { id: 12, name: "Soğan" },
+  { id: 13, name: "Sarımsak" },
+];
+export const puan = { p: 4.2, com: 200, isim: "Position Absolute Acılı Pizza" };
