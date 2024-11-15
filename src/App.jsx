05d7bb7b313d380966308cba5e7fb6e24db16742
Gokcan Parlaktas <gokcanparlaktas@hotmail.com>
2024-11-16 02:31:20 +0300

    Routes and navlinks are added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import SiparisFormu from "./Components/siparisFormu";
 import { Header } from "./Components/Header";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
 function App() {
   const [formData, setFormData] = useState({
     price: 85.5,
@@ -31,9 +31,30 @@ function App() {
         <header className="container-md header flex-col gap-s barlow reset-padding">
           <Header />
         </header>
+
         <main>
           <Switch>
             <Route path="/siparisFormu">
+              <nav className="container-md flex header gap-s barlow reset-padding">
+                <NavLink
+                  to="/"
+                  className={(isActive) =>
+                    "header-link semi-bold" + (!isActive ? " unselected" : "")
+                  }
+                >
+                  Ana Sayfa
+                </NavLink>
+
+                <NavLink
+                  to="/siparisFormu"
+                  className={(isActive) =>
+                    "header-link semi-bold" +
+                    (!isActive ? " header-link semi-bold" : "semi-bold")
+                  }
+                >
+                  Sipariş Formu
+                </NavLink>
+              </nav>
               <SiparisFormu
                 formData={formData}
                 setFormData={setFormData}
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,16 +4,13 @@ import { NavLink } from "react-router-dom";
 export function Header() {
   return (
     <div className="flex-col">
-      <div className=" flex-col text-center">
-        <h1>Teknolojik Yemekler</h1>
-      </div>
-      <div>
-        <a className="header-link" href="#">
-          Ana Sayfa
-        </a>{" "}
-        <a className="header-link semi-bold" href="/siparisFormu.jsx">
-          -{">"} Sipariş Formu
-        </a>
+      <div className=" flex-col text-center barlow">
+        <NavLink
+          to="/"
+          className={(isActive) => "header" + (!isActive ? "header" : "")}
+        >
+          <h1>Teknolojik Yemekler</h1>
+        </NavLink>
       </div>
     </div>
   );
